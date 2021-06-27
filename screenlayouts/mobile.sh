567c5ea4224d0f3e3cd5144a164dcfabba42988d
Kirthihan Yasotharan <kirthihan.yasotharan@hensoldt-cyber.de>
2021-06-28 01:14:25 +0200

    adaptions

--- screenlayouts/mobile.sh
+++ screenlayouts/mobile.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --set TearFree on --output HDMI-A-0 --off --output DisplayPort-0 --off
+#xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --set "TearFree" on --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
---
+++ todo.md
@@ -0,0 +1,11 @@
+configs:
+* dunst
+* gsimplecal
+
+fix screen tearing
+
+fix disappearing tray icons on i3 reload
+
+---
+
+commit, push
