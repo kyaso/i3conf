8d440e6c6a4a02f3ca6526006c3748f8a1d103cd
Kirthihan Yasotharan <kirthihan.yasotharan@hensoldt-cyber.de>
2021-07-07 11:11:12 +0200

    [power] locking before suspend now handled by systemd

--- power/suspend.sh
+++ power/suspend.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
-$HOME/i3conf/power/lock.sh && systemctl suspend
+#$HOME/i3conf/power/lock.sh && systemctl suspend
+
+# Only do this if you have auto locking on suspend enabled in systemd!
+# -> [[20210707105510]]
+systemctl suspend
